370a2e25dffe10c36c672d328005e0a5b7f833ee
LYChubby <f4uzi345@gmail.com>
2025-06-06 15:34:03 +0700

    Membuat BuyerHomeScreen

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -5,6 +5,7 @@ import 'package:canary_template/core/core.dart';
 import 'package:canary_template/data/models/request/auth/login_request_model.dart';
 import 'package:canary_template/presentation/auth/bloc/login/login_bloc.dart';
 import 'package:canary_template/presentation/auth/register_screen.dart';
+import 'package:canary_template/presentation/buyer/profile/buyer_profile_screen.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
